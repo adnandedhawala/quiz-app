cd609327ed1028e195d976eaaf8d8fa6ea1b5648
adnandedhawala <adnan.d@somaiya.edu>
2022-04-15 18:08:47 +0530

    Update quizUsersCard.js

--- components/quizUsersCard.js
+++ components/quizUsersCard.js
@@ -1,7 +1,7 @@
-import {Card} from "antd";
+import {Card, message} from "antd";
 import {useState, useEffect} from "react";
 import {firestore} from "../firebase/firebaseConfig";
-import {collection, getDocs} from "firebase/firestore";
+import {collection, deleteDoc, doc, getDocs} from "firebase/firestore";
 import {CSVLink} from "react-csv";
 
 export const QuizUsersCard = () => {
@@ -22,6 +22,16 @@ export const QuizUsersCard = () => {
     });
   };
 
+  const handleDeleteAll = async () => {
+    if (quizUsers.length > 0) {
+      await quizUsers.map(async (val) => {
+        await deleteDoc(doc(firestore, "quiz_user", String(val.ITS_ID)));
+      });
+      await getQuizUsers();
+      message.success("users deleted");
+    }
+  };
+
   return (
     <Card className="border-radius-10" title="Quiz Users">
       <div className="flex items-center justify-center mb-4">
@@ -32,6 +42,15 @@ export const QuizUsersCard = () => {
         <span className="flex-1">Download User data </span>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 border border-blue-700 rounded h-10 flex items-center justify-center">
           <CSVLink data={quizUsers}>Download me</CSVLink>
+        </button>
+      </div>
+      <div className="flex items-center justify-center mb-4">
+        <span className="flex-1">Clear Users </span>
+        <button
+          onClick={handleDeleteAll}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 border border-blue-700 rounded h-10 flex items-center justify-center"
+        >
+          Delete
         </button>
       </div>
     </Card>
